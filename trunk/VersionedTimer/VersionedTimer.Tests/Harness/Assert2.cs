43b2e5afb071f6b96a304895da40ec26dee7dec4
antiduh <antiduh@8df7111b-2b89-654f-a09e-4e34b830b843>
2018-03-14 05:35:39 +0000

    Added a custom assert class to implement expected exception assertions.

---
+++ trunk/VersionedTimer/VersionedTimer.Tests/Harness/Assert2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VersionedTimer.Tests.Harness
+{
+    public static class Assert2
+    {
+        public static T Throws<T>( Action func ) where T : Exception
+        {
+            T caughtException = null;
+
+            try
+            {
+                func.Invoke();
+            }
+            catch( T e )
+            {
+                caughtException = e;
+            }
+
+            if( caughtException == null )
+            {
+                throw new AssertFailedException(
+                    string.Format( 
+                        "Assert2.Throws failed. No exception was thrown. Expected an exception of type {0} to be thrown.", 
+                        typeof( T ) 
+                    )
+                );
+            }
+
+            return caughtException;
+        }
+    }
+}
