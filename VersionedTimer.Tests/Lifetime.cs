7ef951c490b6dae8b5447533ac6a0aa3df9c60ab
Kevin Thompson <9170708+antiduh@users.noreply.github.com>
2018-03-14 07:29:34 +0000

    Added a test to exercise the constructor argument validation.

---
+++ VersionedTimer.Tests/Lifetime.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VersionedTimer.Tests.Harness;
+
+namespace VersionedTimer.Tests
+{
+    [TestClass]
+    public class Lifetime
+    {
+        /// <summary>
+        /// Verifies the constructor does not abide null callback parameters.
+        /// </summary>
+        [TestMethod]
+        public void Constructor_DoesNotAllow_NullCallbacks()
+        {
+            Assert2.Throws<ArgumentNullException>( () => new VersionedTimer<int>( 0, null ) );
+        }
+    }
+}
