679efa0519e01834ec3500dbd395d0b485bd9d97
Kevin Thompson <9170708+antiduh@users.noreply.github.com>
2018-03-14 07:06:47 +0000

    Added tests for validating the Change methods.

---
+++ VersionedTimer.Tests/Change_Ints.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VersionedTimer.Tests.Harness;
+
+namespace VersionedTimer.Tests
+{
+    /// <summary>
+    /// Verifies the timer supports configuring the timeout.
+    /// </summary>
+    [TestClass]
+    public class Change_Ints
+    {
+        [TestMethod]
+        public void Change_ViaInts_AllowsZero()
+        {
+            SimpleTimerHarness harness = new SimpleTimerHarness();
+            VersionedTimer<int> timer = new VersionedTimer<int>( 123, harness.Callback );
+
+            using( timer )
+            {
+                timer.Change( 0, Timeout.Infinite, 1 );
+                timer.Change( 0, 0, 2 );
+            }
+        }
+
+        [TestMethod]
+        public void Change_ViaInts_AllowsInfinite()
+        {
+            SimpleTimerHarness harness = new SimpleTimerHarness();
+            VersionedTimer<int> timer = new VersionedTimer<int>( 123, harness.Callback );
+
+            using( timer )
+            {
+                timer.Change( 10, Timeout.Infinite, 0 );
+                timer.Change( Timeout.Infinite, Timeout.Infinite, 1 );
+            }
+        }
+
+        [TestMethod]
+        public void Change_ViaInts_DoesNotAllowNegative_Timeout()
+        {
+            SimpleTimerHarness harness = new SimpleTimerHarness();
+            VersionedTimer<int> timer = new VersionedTimer<int>( 123, harness.Callback );
+
+            using( timer )
+            {
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( -2, Timeout.Infinite, 0 );
+                } );
+
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( -3, Timeout.Infinite, 0 );
+                } );
+
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( int.MinValue, Timeout.Infinite, 0 );
+                } );
+            }
+        }
+
+        [TestMethod]
+        public void Change_ViaInts_DoesNotAllowNegative_Period()
+        {
+            SimpleTimerHarness harness = new SimpleTimerHarness();
+            VersionedTimer<int> timer = new VersionedTimer<int>( 123, harness.Callback );
+
+            using( timer )
+            {
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( 0, -2, 0 );
+                } );
+
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( 0, -3, 0 );
+                } );
+
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( 0, int.MinValue, 0 );
+                } );
+            }
+        }
+
+        [TestMethod]
+        public void Change_ViaInts_SingleShot_AllowsPositive()
+        {
+            SimpleTimerHarness harness = new SimpleTimerHarness();
+            VersionedTimer<int> timer = new VersionedTimer<int>( 123, harness.Callback );
+
+            using( timer )
+            {
+                timer.Change( 100, Timeout.Infinite, 1 );
+                timer.Change( 200, Timeout.Infinite, 2 );
+
+                for( int i = 1; i <= 100; i++ )
+                {
+                    timer.Change( 100 + i, Timeout.Infinite, 1 );
+                }
+
+                timer.Change( int.MaxValue, Timeout.Infinite, 1 );
+            }
+        }
+
+
+        [TestMethod]
+        public void Change_ViaInts_MultiShot_AllowsPositive()
+        {
+            SimpleTimerHarness harness = new SimpleTimerHarness();
+            VersionedTimer<int> timer = new VersionedTimer<int>( 123, harness.Callback );
+
+            using( timer )
+            {
+                timer.Change( 100, 100, 1 );
+                timer.Change( 100, 200, 2 );
+
+                for( int i = 1; i <= 100; i++ )
+                {
+                    timer.Change( 100, 100 + i, 1 );
+                }
+
+                timer.Change( 100, int.MaxValue, 1 );
+            }
+        }
+    }
+}
---
+++ VersionedTimer.Tests/Change_TimeSpan.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VersionedTimer.Tests.Harness;
+
+namespace VersionedTimer.Tests
+{
+    /// <summary>
+    /// Verifies the timer supports configuring the timeout.
+    /// </summary>
+    [TestClass]
+    public class Change_TimeSpan
+    {
+        /// <summary>
+        /// A TimeSpan that is slight larger than the maximum supported TimeSpan.
+        /// </summary>
+        private static TimeSpan excessiveSpan;
+
+        /// <summary>
+        /// A TimeSpan that is slightly less negative than -1 ms, eg, -0.999999...990 ms.
+        /// </summary>
+        private static TimeSpan nearOneMsMinus;
+
+        /// <summary>
+        /// A TimeSpan that is slightly more negative than -1 ms, eg, -1.0000..1 ms.
+        /// </summary>
+        private static TimeSpan nearOneMsPlus;
+        static Change_TimeSpan()
+        {
+            var oneTick = TimeSpan.FromTicks( 1 );
+
+            nearOneMsPlus = MSecs( -1 ) + oneTick;
+            nearOneMsMinus = MSecs( -1 ) - oneTick;
+
+            excessiveSpan = VTimeout.MaxTimeout + oneTick;
+        }
+
+        [TestMethod]
+        public void Change_ViaTimeSpan_Allows_Infinite()
+        {
+            SimpleTimerHarness harness = new SimpleTimerHarness();
+            VersionedTimer<int> timer = new VersionedTimer<int>( 123, harness.Callback );
+
+            using( timer )
+            {
+                timer.Change( MSecs( 10 ), Timeout.InfiniteTimeSpan, 0 );
+                timer.Change( Timeout.InfiniteTimeSpan, Timeout.InfiniteTimeSpan, 1 );
+            }
+        }
+
+        [TestMethod]
+        public void Change_ViaTimeSpan_Allows_Zero()
+        {
+            SimpleTimerHarness harness = new SimpleTimerHarness();
+            VersionedTimer<int> timer = new VersionedTimer<int>( 123, harness.Callback );
+
+            using( timer )
+            {
+                timer.Change( TimeSpan.Zero, Timeout.InfiniteTimeSpan, 1 );
+                timer.Change( TimeSpan.Zero, TimeSpan.Zero, 2 );
+            }
+        }
+        [TestMethod]
+        public void Change_ViaTimeSpan_DoesNotAllow_ExcessiveSpan()
+        {
+            SimpleTimerHarness harness = new SimpleTimerHarness();
+            VersionedTimer<int> timer = new VersionedTimer<int>( 123, harness.Callback );
+
+            using( timer )
+            {
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( excessiveSpan, Timeout.InfiniteTimeSpan, 0 );
+                } );
+
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( TimeSpan.Zero, excessiveSpan, 0 );
+                } );
+
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( excessiveSpan, excessiveSpan, 0 );
+                } );
+            }
+        }
+
+        [TestMethod]
+        public void Change_ViaTimeSpan_DoesNotAllow_Negative_Period()
+        {
+            SimpleTimerHarness harness = new SimpleTimerHarness();
+            VersionedTimer<int> timer = new VersionedTimer<int>( 123, harness.Callback );
+
+            using( timer )
+            {
+                // Smallest negative possible.
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( TimeSpan.Zero, TimeSpan.FromTicks( -1 ), 0 );
+                } );
+
+                // Edge case near -1 ms (a special value), low side.
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( TimeSpan.Zero, nearOneMsPlus, 0 );
+                } );
+
+                // Edge case near -1 ms (a special value), high side.
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( TimeSpan.Zero, nearOneMsMinus, 0 );
+                } );
+
+                // More negative.
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( TimeSpan.Zero, MSecs( -2 ), 0 );
+                } );
+            }
+        }
+
+        [TestMethod]
+        public void Change_ViaTimeSpan_DoesNotAllow_Negative_Timeout()
+        {
+            SimpleTimerHarness harness = new SimpleTimerHarness();
+            VersionedTimer<int> timer = new VersionedTimer<int>( 123, harness.Callback );
+
+            using( timer )
+            {
+                // Smallest negative possible.
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( TimeSpan.FromTicks( -1 ), Timeout.InfiniteTimeSpan, 0 );
+                } );
+
+                // Edge case near -1 ms (a special value), low side.
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( nearOneMsPlus, Timeout.InfiniteTimeSpan, 0 );
+                } );
+
+                // Edge case near -1 ms (a special value), high side.
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( nearOneMsMinus, Timeout.InfiniteTimeSpan, 0 );
+                } );
+
+                // More negative.
+                Assert2.Throws<ArgumentException>( () =>
+                {
+                    timer.Change( MSecs( -2 ), Timeout.InfiniteTimeSpan, 0 );
+                } );
+            }
+        }
+        [TestMethod]
+        public void Change_ViaTimeSpan_MultiShot_Allows_Positive()
+        {
+            SimpleTimerHarness harness = new SimpleTimerHarness();
+            VersionedTimer<int> timer = new VersionedTimer<int>( 123, harness.Callback );
+
+            using( timer )
+            {
+                timer.Change( MSecs( 100 ), MSecs( 100 ), 1 );
+                timer.Change( MSecs( 100 ), MSecs( 200 ), 1 );
+
+                for( int i = 1; i <= 100; i++ )
+                {
+                    timer.Change( MSecs( 100 ), MSecs( 100 + i ), 1 );
+                }
+
+                timer.Change( MSecs( 100 ), MSecs( int.MaxValue ), 1 );
+            }
+        }
+
+        [TestMethod]
+        public void Change_ViaTimeSpan_SingleShot_Allows_Positive()
+        {
+            SimpleTimerHarness harness = new SimpleTimerHarness();
+            VersionedTimer<int> timer = new VersionedTimer<int>( 123, harness.Callback );
+
+            using( timer )
+            {
+                timer.Change( MSecs( 100 ), Timeout.InfiniteTimeSpan, 1 );
+                timer.Change( MSecs( 200 ), Timeout.InfiniteTimeSpan, 1 );
+
+                for( int i = 1; i <= 100; i++ )
+                {
+                    timer.Change( MSecs(100 + i), Timeout.InfiniteTimeSpan, 1 );
+                }
+
+                timer.Change( MSecs( int.MaxValue ), Timeout.InfiniteTimeSpan, 1 );
+
+                timer.Change( VTimeout.MaxTimeout, Timeout.InfiniteTimeSpan, 1 );
+                timer.Change( TimeSpan.Zero, VTimeout.MaxTimeout, 1 );
+                timer.Change( VTimeout.MaxTimeout, VTimeout.MaxTimeout, 1 );
+            }
+        }
+        private static TimeSpan MSecs( double value )
+        {
+            return TimeSpan.FromMilliseconds( value );
+        }
+    }
+}
